142a06e23f86b4316abf318c9e1d798b1c0eaf3c
Jeff Palm <jeffpalm@gmail.com>
2018-08-28 08:23:17 -0400

    fixed double backgrounds

--- tools/commit.sh
+++ tools/commit.sh
@@ -6,6 +6,5 @@ set -e
 
 git add .
 git commit -am "$@"
-#git remote add origin https://github.com/spudtrooper/twimmage-android.git
-git remote set-url origin https://jeffpalm@bitbucket.org/jeffpalm/twimmage-web.git
+git remote add origin https://github.com/spudtrooper/twimmage-android.git
 git push -u origin master
